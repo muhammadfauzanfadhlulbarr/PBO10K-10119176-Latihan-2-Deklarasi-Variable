f522a39540bea8ea0f24ed4924fa64b9eca3a8d2
Muhammad Fauzan <muhammadfauzanfadhlulbarr@gmail.com>
2020-10-19 10:39:51 +0700

    first commit

---
+++ Latihan2.java
@@ -0,0 +1,29 @@
+/**
+ * Latihan2
+ */
+public class Latihan2 {
+    
+    public static void main(String[] args) {
+        // Deklarasi variable
+        int nilaiInt;
+        final double PHI = 3.14;
+        boolean nilaiLogika;
+        char nilaiKarakter;
+
+     // Memberi nilai ke variable
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+
+    
+    
+    // Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi Variable nilai = " + nilaiInt);
+        System.out.println("Isi Variable PHI = " + PHI);
+        System.out.println("Isi Variable logika = " + nilaiLogika);
+        System.out.println("Isi Variable karakter = " + nilaiKarakter);
+
+    }
+
+}
